225267f01b73421f90fb210d4b097e607bda69f1
Pryhym30033 <pryhymcapitalmanagement@gmail.com>
2025-01-15 17:06:25 -0500

    game loops

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main ():
     dt = 0
     x = SCREEN_WIDTH/2
     y = SCREEN_HEIGHT/2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x, y)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
@@ -17,11 +20,14 @@ def main ():
         dt = clock.tick()/1000
         clock.tick(60)
         screen.fill((0, 0, 0))
-        player.draw(screen)
+
+        for item in updatable:
+            item.update(dt)
+
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
