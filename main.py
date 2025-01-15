a5c48cb59c2f19585006ff16fb38b3b8c86cb88e
Pryhym30033 <pryhymcapitalmanagement@gmail.com>
2025-01-14 19:02:28 -0500

    triangle ship

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import pygame
 from constants import *
+from player import Player
 
 def main ():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH/2
+    y = SCREEN_HEIGHT/2
+    player = Player(x, y)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
-            
+               return
+        dt = clock.tick()/1000
+        clock.tick(60)
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
